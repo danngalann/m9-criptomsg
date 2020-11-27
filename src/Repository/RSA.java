ee417ee6d4b6a2f652bfe11fac16f86ed90aa34b
danngalann <47415745+danngalann@users.noreply.github.com>
2020-11-27 15:38:57 +0100

    Added RSA class

---
+++ src/Repository/RSA.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Repository;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+import javax.crypto.Cipher;
+
+/**
+ *
+ * @author Daniel Galán
+ */
+public class RSA {
+    
+    public Map<String, Object> makeKeys() throws Exception {
+        KeyPairGenerator keyPairGenerator =  KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        
+        KeyPair keyPair = keyPairGenerator.genKeyPair();
+        PrivateKey privateKey = keyPair.getPrivate();
+        PublicKey publicKey =  keyPair.getPublic();
+        
+        Map<String, Object> keys = new HashMap<String, Object>();
+        keys.put("private", privateKey);
+        keys.put("public", publicKey);
+        
+        return keys;
+    }
+    
+    public String decrypt(String eText, PublicKey publicKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, publicKey);
+        return new String(cipher.doFinal(Base64.getDecoder().decode(eText)));
+    }
+    
+    public String encrypt(String pText, PrivateKey privateKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+        return Base64.getEncoder().encodeToString(cipher.doFinal(pText.getBytes()));
+    }
+    
+}
